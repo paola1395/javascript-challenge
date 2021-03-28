e20e97c1b7903985abc41bf755546669a71dedc5
Paola Lopez <paola.lopez1@yahoo.com>
2021-03-27 22:58:42 -0700

    created app.js, selected table data/body and appended data to the body

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,16 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Use D3 to select data table body
+var tableBody = d3.select("tbody")
+
+// Append data to the table body
+tableData.forEach(data => {
+    console.log(data);
+    var row = tableBody.append("jr");
+    Object.defineProperties(data).forEach(([key, value]) =>{
+        console.log(key, value);
+        row.append("td").text(value)
+    });
+});
+
